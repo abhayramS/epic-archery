4306661f4def33b8704f9642f34e486083ef1b80
abhayramS <89374590+abhayramS@users.noreply.github.com>
2022-11-22 19:07:21 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -5,36 +5,70 @@ const Constraint = Matter.Constraint;
 
 var engine, world;
 var canvas;
+var palyer, playerBase, playerArcher;
+var arrow;
+var baseimage;
+var playerimage;
+var playerArrows = [];
 
+
+function preload() {
+  backgroundImg = loadImage("./assets/background.png");
+  baseimage = loadImage("./assets/base.png");
+  playerimage = loadImage("./assets/player.png");
+}
 
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
-  
-   //Initialising Engine
+
   engine = Engine.create();
   world = engine.world;
-	
-   //Create Player Base and Computer Base Object
 
+  angleMode(DEGREES);
 
- }
+  var options = {
+    isStatic: true
+  };
+
+  playerBase = Bodies.rectangle(200, 350, 180, 150, options);
+  World.add(world, playerBase);
+
+  player = Bodies.rectangle(250, playerBase.position.y - 160, 50, 180, options);
+  World.add(world,player)
+
+  playerArcher = new PlayerArcher(
+    340,
+    playerBase.position.y - 112,
+    120,
+    120
+  );
+  arrow = new PlayerArrow(
+    playerArcher.body.position.x,
+    playerArcher.body.position.y,
+    100,
+    10
+  );
+}
 
 function draw() {
-
-  background(180);
+  background(backgroundImg);
 
   Engine.update(engine);
+  image(baseimage,playerBase.position.x,playerBase.position.y,180,150)
+  image(playerimage,player.position.x,player.position.y,50,180)
 
+  playerArcher.display();
+  arrow.display();
+
+  if (keyCode === 32) {
+    arrow.shoot(playerArcher.body.angle);
+  }
   // Title
   fill("#FFFF");
   textAlign("center");
   textSize(40);
   text("EPIC ARCHERY", width / 2, 100);
-
-   //Display Playerbase and computer base 
-
-
-   //display Player and computerplayer
-
-
 }
+
+
+
